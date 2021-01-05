7d31b907e805149e2f828694d1a7d3c23f0413c7
Joel David Hernandez Cruz <jdavidhc1710@gmail.com>
2021-01-05 16:37:07 -0500

    fix deface tempaltes

--- app/overrides/add_insights_to_admin_side_menu.html.rb
+++ app/overrides/add_insights_to_admin_side_menu.html.rb
@@ -10,6 +10,6 @@ else
     virtual_path: 'spree/admin/shared/_main_menu',
     name: 'add_insights_to_admin_side_menu',
     insert_bottom: 'nav',
-    partial: 'spree/admin/shared/_insights_side_menu',
+    partial: 'spree/admin/shared/insights_side_menu',
   )
 end
